ef670eb3f3fea23b9a95becfcb670cf8f18cde8f
Özge <ozge.sakarya@outlook.com>
2026-01-15 23:25:36 +0100

    price calculator done

--- sitzplan.js
+++ sitzplan.js
@@ -1,6 +1,5 @@
 /**
  * TODO:
- *  preisrechnung
  *  zufällige ausverkaufte sitze
  *  download funktion
  *  farben ändern
@@ -8,10 +7,22 @@
  */
 
 (() => {
-    const createSeat = (seatType) => {
+    const preisRechnung = document.querySelector("#preisrechnung");
+    const tickets = [];
+    const ticketPreis = {
+        vip: 99.99,
+        regular: 79.99,
+        standing: 59.99
+    };
+
+
+
+    const createSeat = (groupType) => {
         const seat = document.createElement("div");
-        seat.classList.add("seat", seatType);
-        seat.addEventListener("click", toggleSelection);
+        seat.classList.add("seat", groupType);
+        seat.addEventListener("click", () => {
+            toggleSelection(seat, groupType);
+        });
         return seat;
 
     }
@@ -22,14 +33,28 @@
             seatGroup.appendChild(seat);
         }
     }
-    function toggleSelection () {
-       const isSelected = this.classList.contains("selected")
-       if (isSelected){
-        this.classList.remove("selected")
-       }
-       else{
-        this.classList.add("selected")
-       }
+
+    const calculatePrice = () => {
+        const total = tickets.reduce((sum, ticket) => {
+            return sum + ticketPreis[ticket.groupType];
+        }, 0);
+        preisRechnung.innerHTML = total.toFixed(2) + " €";
+    }
+
+    function toggleSelection(seat, groupType) {
+        const isSelected = seat.classList.contains("selected")
+        if (isSelected) {
+            seat.classList.remove("selected");
+            let foundItemIndex = tickets.findIndex((ticket) => ticket.groupType === groupType
+            );
+            tickets.splice(foundItemIndex, 1);
+        }
+        else {
+            seat.classList.add("selected");
+            tickets.push({ groupType });
+        }
+
+        calculatePrice()
     }
 
 
